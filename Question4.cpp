f3fb12a586000fcbc929276f8ffb14097f45166a
ShashwatkumarXD <shashwat200418@gmail.com>
2024-02-19 16:15:24 +0530

    Pattern Question

---
+++ Question4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main(){
+    int row=5;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";// printf("*");
+        }
+    cout<<endl;// printf("\n");     
+    }
+    return 0;
+}
+
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int a[]={1,2,3,4,5,6,7};
-    int n=a.size();
-    for(int i=0;i<n;i++){
+cout<<endl;
