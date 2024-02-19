7169cdb23a2c7b9a216300ce6f21ca2b28e2e150
ShashwatkumarXD <shashwat200418@gmail.com>
2024-02-19 16:48:47 +0530

    Patter Questions

--- Question4.cpp
+++ Question4.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <stdio.h>
+#include <string>
 
 using namespace std;
-int main(){
-    int row=5;
+void star(int row ){
     for(int i=0;i<row;i++){
         for(int j=0;j<=i;j++){
             cout<<"*";// printf("*");
         }
     cout<<endl;// printf("\n");     
     }
+}
+void number(int a){
+    for(int i=0;i<a;i++){
+        for(int j=0;j<=i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+void alphabate(int b){
+    for(int i=0;i<b;i++){
+        for(int j=0;j<=i;j++){
+            cout<<static_cast<char>('A'+j)<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int x=5;
+    star(x);
+    cout<<star<<endl;
+    number(x);
+    cout<<number<<endl;
+    alphabate(x);
+    cout<<alphabate<<endl;
     return 0;
 }
+
+
 
 
