2daff5ef5f5515eda8bc2726174082c8bb25e47d
ShashwatkumarXD <shashwat200418@gmail.com>
2024-02-24 09:34:25 +0530

    Printing Array using Pointers

---
+++ Question7_Pointer.cpp
@@ -0,0 +1,15 @@
+// Printing Array using Pointers
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A[5]={2,4,6,8,10};
+    int *p;
+    p=A;
+    for(int i=0;i<5;i++)
+    cout<<p[i]<<endl;
+    return 0;
+}
